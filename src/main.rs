960f3a8f056e7c6fb79b94d91b20163bfa04185f
yinyanlv <yanlv.yin@gmail.com>
2017-04-18 18:24:12 +0800

    chore: 测试channel

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 extern crate iron;
 
+use std::thread;
+use std::sync::mpsc;
+
 use iron::prelude::*;
 use iron::status;
 
 fn main() {
 
-    Iron::new(|_: &mut Request| {
-        Ok(Response::with((
-            status::Ok,
-            "Hello Iron"
-        )))
-    }).http("localhost:3000").unwrap();
+    let (sender, receiver): (mpsc::Sender<i32>, mpsc::Receiver<i32>) = mpsc::channel();
+
+    thread::spawn(move || {
+        println!("before action");
+        sender.send(123).unwrap();
+        println!("after action");
+    });
+
+    // Iron::new(|_: &mut Request| {
+    //     Ok(Response::with((
+    //         status::Ok,
+    //         "Hello Iron"
+    //     )))
+    // }).http("localhost:3000").unwrap();
+
+    println!("prev");
+    println!("recevie {}", receiver.recv().unwrap());
+    println!("next");
 }
