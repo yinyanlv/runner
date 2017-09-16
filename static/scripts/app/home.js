2704b939d478d1101acb497eccb702a38acb16af
bugong <1761869346@qq.com>
2017-09-16 23:09:33 +0800

    优化scss，适配移动端

--- static/scripts/app/home.js
+++ static/scripts/app/home.js
@@ -1,3 +1,29 @@
 $(function () {
+  var app = {
+    init: function () {
+      var self = this;
 
+      self.initElements();
+      self.initEvents();
+    },
+
+    initElements: function () {
+      var self = this;
+
+      self.$mBtnMenu = $('#m-btn-menu');
+      self.$mNav = $('#m-nav');
+
+    },
+
+    initEvents: function() {
+      var self = this;
+
+      self.$mBtnMenu.on('click', function () {
+
+        self.$mNav.is(':visible') ? self.$mNav.hide() : self.$mNav.show();
+      });
+    }
+  };
+
+  app.init();
 });
