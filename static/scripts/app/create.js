6fa4afc9cb7e2b7df47c18c5cbbd0f4c5de73d20
bugong <1761869346@qq.com>
2017-09-18 23:18:24 +0800

    增加关于本站

---
+++ static/scripts/app/create.js
@@ -0,0 +1,32 @@
+$(function () {
+  var create = {
+    init: function () {
+      var self = this;
+
+      self.initPlugins();
+      self.initElements();
+      self.initEvents();
+    },
+
+    initPlugins: function () {
+      var self = this;
+      var editor = new Editor();
+
+      editor.render($('.editor')[0]);
+    },
+
+    initElements: function () {
+      var self = this;
+
+
+    },
+
+    initEvents: function() {
+      var self = this;
+
+
+    }
+  };
+
+  create.init();
+});
