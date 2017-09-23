da0e1ffe5f06a4ed70b6a061861778f0f3482798
bugong <1761869346@qq.com>
2017-09-23 08:03:31 +0800

    begin register

--- static/scripts/app/register.js
+++ static/scripts/app/register.js
@@ -11,12 +11,74 @@ $(function () {
         initElements: function () {
             var self = this;
 
-
+            self.$inputUsername = $('#username');
+            self.$inputPassword = $('#password');
+            self.$inputRepeatPassword = $('#repeat-password');
+            self.$inputEmail = $('#email');
+            self.$btnRegister = $('#btn-register');
+            self.$needCheckInputs = $('.input-line input');
         },
 
         initEvents: function () {
             var self = this;
 
+            self.$btnRegister.on('click', function () {
+
+                self.register();
+            });
+        },
+
+        register: function () {
+            var self = this;
+
+            if (!self.isValid()) return;
+
+            var params = self.getParams();
+
+
+            $.ajax({
+                url: globalConfig.path + '/register',
+                type: 'POST',
+                dataType: 'application/json',
+                data: params,
+                success: function () {
+
+                },
+                error: function () {
+
+                }
+            });
+        },
+
+        getParams: function () {
+            var self = this;
+
+            return {
+                username: $.trim($inputUsername.val()),
+                password: $.trim($inputPassword.val()),
+                email: $.trim($inputEmail.val())
+            };
+        },
+
+        isValid: function() {
+            var self = this;
+            var isValid = true;
+
+            for (var i = 0; i < self.$needCheckInputs.length; i++) {
+
+                if (!self.checkInput(self.$needCheckInputs[i])) {
+
+                    isValid = false;
+                    break;
+                }
+            }
+
+            return isValid;
+        },
+
+        checkInput: function (input) {
+            var self = this;
+            var $input = $(input);
 
         }
     };
